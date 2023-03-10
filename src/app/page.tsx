f8916d1231ece1a91747c386bfd27dda4ff37381
simon198 <c121549443@gmail.com>
2023-03-10 16:12:46 +0800

    add divider in blog list page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,21 @@ export default async function Home() {
     <section>
       <h1 className="font-bold text-3xl font-serif mb-5">Blog</h1>
       {sortedPost.map((post) => (
-        <Link
-          key={post.slug}
-          className="flex flex-col space-y-1 mb-4"
-          href={`/blog/${post.slug}`}
-        >
-          <div className="w-full flex flex-col">
-            <p className="font-bold">{post.title}</p>
-            <p className="text-sm line-clamp-2">{post.description}</p>
-            {post.tags && <Tags tags={post.tags} />}
-            <p className="font-bold text-sm">{"(繼續閱讀...)"}</p>
-          </div>
-        </Link>
+        <>
+          <Link
+            key={post.slug}
+            className="flex flex-col space-y-1"
+            href={`/blog/${post.slug}`}
+          >
+            <div className="w-full flex flex-col">
+              <p className="font-bold">{post.title}</p>
+              <p className="text-sm line-clamp-2">{post.description}</p>
+              {post.tags && <Tags tags={post.tags} />}
+              <p className="font-bold text-sm">{"(繼續閱讀...)"}</p>
+            </div>
+          </Link>
+          <div className="h-[0.1em] bg-neutral-50 dark:bg-neutral-800 m-4" />
+        </>
       ))}
     </section>
   );
