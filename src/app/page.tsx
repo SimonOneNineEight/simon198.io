cbeb7f2b0e38234dcbd712dbaadfa89fea62867b
simon198 <c121549443@gmail.com>
2023-05-23 10:57:30 +0800

    HOME PAGE: add introduction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,6 +55,14 @@ export default async function HomePage() {
       </div>
       <p className="my-5 max-w-[600px] text-neutral-800 dark:text-neutral-200">
         {/* TODO: need to add some introduction */}
+        {
+          "我是Simon198，一個大學念經濟卻意外觸碰到寫程式所以半路出家的軟體工程師。曾經擔任過前端工程師，目前正準備要前往美國東北大學唸碩士，期望可以在美國撈到很多桶金XD。"
+        }
+        <br />
+        <br />
+        {
+          "這個網站主要會作為我在這一路上學到的各種東西的紀錄，希望可以讓和我走相似道路的人可以有一個參考，有任何疑問也歡迎直接 DM 我，我會熱情地回答你的!"
+        }
       </p>
       {/* <ul className="flex flex-col md:flex-row mt-8 space-x-0 md:space-x-4 space-y-2 md:space-y-0 font-sm text-neutral-500 dark:text-neutral-400">
         <li>
