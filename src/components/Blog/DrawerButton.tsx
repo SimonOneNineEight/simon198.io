f319163086616ec4863b6f36d8fe4c3c0fadb681
simon198 <c121549443@gmail.com>
2023-06-23 11:28:06 +0800

    BLOG PAGE: adjust blog filter drawer trigger button

--- src/components/Blog/DrawerButton.tsx
+++ src/components/Blog/DrawerButton.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+import IconArrowUp from "@/images/icon-arrow-up.svg";
+import IconArrowDown from "@/images/icon-arrow-down.svg";
+
 const DrawerButton = ({
   isDrawerOpen,
   setDrawerOpen,
@@ -8,11 +12,15 @@ const DrawerButton = ({
   return (
     <div className="text-center">
       <button
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 relative"
         type="button"
         onClick={() => setDrawerOpen((previous) => !previous)}
       >
-        {isDrawerOpen ? "up" : "down"}
+        <Image
+          src={isDrawerOpen ? IconArrowUp : IconArrowDown}
+          alt={isDrawerOpen ? "Up" : "Down"}
+          width={24}
+          height={24}
+        />
       </button>
     </div>
   );
